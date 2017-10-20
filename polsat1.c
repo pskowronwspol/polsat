f305ac3ffa38183fdc9ed1c5f8376ef8064a3216
pskowronwspol <piotr.skowron101@onet.pl>
2017-10-20 09:38:48 +0200

    implementacja pierwszych programoww jezyku c

---
+++ polsat1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> /*bibloiteka standardowa input-output*/
+
+/* y = f(x) */
+/*     f() => 0 */
+int main()/* funkcja glowna*/
+/* typ zwracanej wartosci (int - wartosc calkowita */
+{
+	puts("polsat"); /*puts - funkcja z biblioteki standardowej*/
+			/*wyswietlajaca ciag znakow*/
+	return 0;
+
+}
